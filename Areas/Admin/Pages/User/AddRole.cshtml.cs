db1f4ef94d0dd201e29d3f973094c9ed51eb8ea8
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2022-01-16 11:39:16 +0700

    Users List 3: Update User Role

---
+++ Areas/Admin/Pages/User/AddRole.cshtml.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorEF.Models;
+
+namespace RazorEF.Areas.Admin.Pages.User
+{
+    public class AddRoleModel : PageModel
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AddRoleModel(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public AppUser User { get; set; }
+
+        [BindProperty]
+        [Display(Name = "Roles")]
+        public string[] RoleNames { get; set; }
+        public SelectList AllRoles { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string userId)
+        {
+            if (string.IsNullOrEmpty(userId)) return NotFound("User ID not found");
+
+            User = await _userManager.FindByIdAsync(userId);
+            if (User == null)
+            {
+                return NotFound("Unable to load user.");
+            }
+
+            // 18.1 Get RolesName of user
+            RoleNames = (await _userManager.GetRolesAsync(User)).ToArray<string>();
+
+            // 18.1 Get AllRoles from database
+            List<string> roleNames = await _roleManager.Roles.Select(r => r.Name).ToListAsync();
+            AllRoles = new SelectList(roleNames);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string userId)
+        {
+            if (string.IsNullOrEmpty(userId)) return NotFound("User ID not found");
+
+            User = await _userManager.FindByIdAsync(userId);
+            if (User == null)
+            {
+                return NotFound("Unable to load user.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // 18.3 Delete and add roles
+            var oldRoleNames = (await _userManager.GetRolesAsync(User)).ToArray<string>();
+            // old: [Vip, Editor]
+            // new: [Admin, Editor]
+            var deleteRoles = oldRoleNames.Where(r => !RoleNames.Contains(r));
+            var addRoles = RoleNames.Where(r => !oldRoleNames.Contains(r));
+
+            var resultDelete = await _userManager.RemoveFromRolesAsync(User, deleteRoles);
+            if (!resultDelete.Succeeded)
+            {
+                resultDelete.Errors.ToList().ForEach(error =>
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                });
+                return Page();
+            }
+
+            var resultAdd = await _userManager.AddToRolesAsync(User, addRoles);
+            if (!resultAdd.Succeeded)
+            {
+                resultAdd.Errors.ToList().ForEach(error =>
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                });
+                return Page();
+            }
+
+            StatusMessage = $"Updated role for user: {User.UserName}";
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
