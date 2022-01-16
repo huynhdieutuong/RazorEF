004ffb9c7eba12489eb529d3db127dc7a794ce75
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2022-01-16 13:02:32 +0700

    Role-based Authorization

--- Areas/Admin/Pages/Role/Index.cshtml.cs
+++ Areas/Admin/Pages/Role/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,21 @@ using RazorEF.Models;
 
 namespace RazorEF.Areas.Admin.Pages.Role
 {
+    [Authorize(Roles = "Admin")] // "Admin,Vip,Editor" => Admin || Vip || Editor
+    // [Authorize(Roles = "Vip")]
+    // [Authorize(Roles = "Editor")]
+    // => Admin && Vip && Editor
+
+    /*
+        When update user role, user need logout and login to affect
+        If want auto affect, use this code:
+        services.Configure<SecurityStampValidatorOptions>(options =>
+        {
+            // Trên 30 giây truy cập lại sẽ nạp lại thông tin User (Role)
+            // SecurityStamp trong bảng User đổi -> nạp lại thông tinn Security
+            options.ValidationInterval = TimeSpan.FromSeconds(30);
+        });
+    */
     public class IndexModel : RolePageModel
     {
         public IndexModel(RoleManager<IdentityRole> roleManger, MyBlogContext context) : base(roleManger, context)
